a873de2f279850f72d9d0ea8479aed2e45ce2f64
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-14 16:47:14 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
+
 """
-Pagination Module
-Server class
+Return a tuple of size two containing a start index and an end index
 """
+
 import csv
 import math
-from typing import List, TypedDict, Optional
+from typing import Tuple, List
 
 
-Hyper = TypedDict('Hyper', {
-    "page_size": int,
-    "page": int,
-    "data": List[List],
-    "next_page": Optional[int],
-    "prev_page": Optional[int],
-    "total_pages": int
-})
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    Return a tuple of size two containing
+    a start index and an end index
+    """
+    start = (page - 1) * page_size
+    end = page * page_size
+    return (start, end)
 
 
 class Server:
@@ -38,34 +39,31 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ get paginated data with a given page and page_size """
+        """returns data"""
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
+
         start, end = index_range(page, page_size)
         end = min(end, len(self.dataset()))
+
         if start > len(self.dataset()):
             return []
+
         return self.__dataset[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> Hyper:
-        """ Returns details of the pagination """
-        data = self.get_page(page, page_size)
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and isinstance(page_size, int)
+        assert page > 0 and page_size > 0
+
+        data = self.dataset()
         start, end = index_range(page, page_size)
         end = min(end, len(self.dataset()))
-        hypers: Hyper = {
-            "page_size": page_size,
-            "page": page,
-            "data": data,
-            "next_page": page + 1 if end != len(self.dataset()) else None,
-            "prev_page": page - 1 if start != 0 else None,
-            "total_pages": math.ceil(len(self.dataset()) / page_size)
-        }
 
-        return hypers
-
-
-def index_range(page: int, page_size: int) -> tuple:
-    """ return a start index and an end index corresponding to the range """
-    start = sum([page_size for i in range(page - 1)]) if page > 0 else 0
-    end = page_size + start
-    return (start, end)
+        total_pages = math.ceil(len(data) / page_size)
+        next_page = (page + 1) if page < total_pages else None
+        prev_page = (page - 1) if page >= 1 else None
+        page_info = {"page_size": page_size, "page": page,
+                     "data": self.get_page(page, page_size),
+                     "next_page": next_page, "prev_page": prev_page,
+                     "total_pages": total_pages}
+        return page_info
