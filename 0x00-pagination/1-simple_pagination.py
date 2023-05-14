601c87ff45d33678212777024ead3a75dfd5c175
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-14 15:51:34 +0300

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -39,11 +39,14 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """ get paginated data with a given page and page_size """
         assert isinstance(page, int) and isinstance(page_size, int)
         assert page > 0 and page_size > 0
+
+        data = self.dataset()
         start, end = index_range(page, page_size)
-        end = min(end, len(self.dataset()))
-        if start > len(self.dataset()):
+        end = min(end, len(data))
+
+        if start > len(data):
             return []
-        return self.__dataset[start:end]
+
+        return data[start:end]
