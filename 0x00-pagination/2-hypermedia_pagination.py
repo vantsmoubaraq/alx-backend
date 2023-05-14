6606bbc32673f921d5e02a44436098ca7dc7c5cc
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-14 16:42:48 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -51,18 +51,18 @@ class Server:
 
         return self.__dataset[start:end]
 
-    def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
-
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Hyper:
+        """ Returns details of the pagination """
+        data = self.get_page(page, page_size)
         start, end = index_range(page, page_size)
         end = min(end, len(self.dataset()))
+        hypers: Hyper = {
+            "page_size": page_size,
+            "page": page,
+            "data": data,
+            "next_page": page + 1 if end != len(self.dataset()) else None,
+            "prev_page": page - 1 if start != 0 else None,
+            "total_pages": math.ceil(len(self.dataset()) / page_size)
+        }
 
-        total_pages = math.ceil(len(data) / page_size)
-        next_page = (page + 1) if page < total_pages else None
-        prev_page = (page - 1) if page >= 1 else None
-        page_info = {"page_size": page_size, "page": page,
-                     "data": self.get_page(page, page_size),
-                     "next_page": next_page, "prev_page": prev_page,
-                     "total_pages": total_pages}
-        return page_info
+        return hypers
