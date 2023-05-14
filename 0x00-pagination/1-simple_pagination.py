92c5596096c4637f031308e3413922f87e2d69ec
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-14 15:37:35 +0300

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -47,7 +47,7 @@ class Server:
         data = self.dataset()
 
         for value in ind:
-            if value < 0 or value >= len(data):
+            if value < 0 or value > len(data):
                 return []
 
         return data[ind[0]:ind[1]]
