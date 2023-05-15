8a425322e9f4ffb833028842a19d5cc0465a7bc9
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-15 07:57:45 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,6 +52,7 @@ class Server:
         return self.__dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """returns dict with page info"""
         data = self.get_page(page, page_size)
 
         total_pages = math.ceil(len(self.dataset()) / page_size)
