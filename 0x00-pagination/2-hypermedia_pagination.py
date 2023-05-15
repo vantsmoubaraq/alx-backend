4ba3f529827efec829af7c76e4f441be2310b4b8
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-15 07:54:32 +0300

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,9 +52,6 @@ class Server:
         return self.__dataset[start:end]
 
     def get_hyper(self, page: int = 1, page_size: int = 10) -> List[List]:
-        assert isinstance(page, int) and isinstance(page_size, int)
-        assert page > 0 and page_size > 0
-
         data = self.get_page(page, page_size)
 
         total_pages = math.ceil(len(self.dataset()) / page_size)
