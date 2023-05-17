1148a60b45c13f1d90fe84b7179ffb5b0941731b
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-17 14:36:01 +0300

    caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -23,7 +23,7 @@ class MRUCache(BaseCaching):
             recent_key = key
 
         if len(self.cache_data) > self.MAX_ITEMS:
-            min_time = datetime.datetime(year=1000, month=1, day=1)
+            min_time = datetime.datetime(year=1, month=1, day=1)
             diff = recent - min_time
             second_recent = None
             for key, value in self.time_stamp.items():
