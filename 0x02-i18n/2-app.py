70fa34407a8cd3daeaeb4e09df415fcb3d11dc0d
vantsmoubaraq <vantsmoubaraq@gmail.com>
2023-05-23 12:23:51 +0300

    i18n

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -25,13 +25,14 @@ def welcome():
     """renders 2-index.html"""
     return render_template("2-index.html")
 
+
 @babel.localeselector
 def get_locale():
     """get best match"""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
-babel.init_app(app, locale_selector=get_locale)
+"""babel.init_app(app, locale_selector=get_locale)"""
 
 
 if __name__ == "__main__":
